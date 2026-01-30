9c29cefa8306b435963bc76caf00b554176cf52e
AnuragKrVerma <211006@kit.ac.in>
2026-01-30 15:04:06 +0530

    03 Authentication

---
+++ server/lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "../lib/prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    basePath: "/api/auth",
+    trustedOrigins: [process.env.FRONTEND_URL || "http://localhost:3000"],
+    socialProviders:{
+        github:{
+            clientId: process.env.GITHUB_CLIENT_ID || "",
+            clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+        }
+    },
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { toNodeHandler} from 'better-auth/node';
+import { auth } from '../lib/auth';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // Specify allowed origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use(express.json());
+
+
 app.get('/health', (req, res) => {
   res.send('Server is healthy');
 });
+
 
 
 const PORT = process.env.PORT || 3005;
